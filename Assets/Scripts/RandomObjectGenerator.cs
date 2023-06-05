6f31cfda0849d1bfab456ffb40a5e36413c18022
Muhammad Fariz <muhammadfarizh31@gmail.com>
2023-06-05 16:57:09 +0700

    init AR feature

---
+++ Assets/Scripts/RandomObjectGenerator.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class RandomObjectGenerator : MonoBehaviour
+{
+    public ARRaycastManager raycastManager; // AR Raycast Manager component
+    public GameObject[] objectsToSpawn; // Array of objects to spawn
+    public float spawnInterval = 2f; // Time interval between spawns
+
+    private float spawnTimer = 0f; // Timer for tracking spawn interval
+
+    void Update()
+    {
+        // Check if enough time has passed for the next spawn
+        if (Time.time >= spawnTimer)
+        {
+            // Perform raycast from the center of the screen
+            Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            raycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon);
+
+            // Check if any planes are detected
+            if (hits.Count > 0)
+            {
+                // Get a random plane hit result
+                ARRaycastHit hit = hits[Random.Range(0, hits.Count)];
+
+                // Spawn a random object on the detected plane
+                SpawnRandomObject(hit.pose.position);
+
+                // Reset the timer for the next spawn
+                spawnTimer = Time.time + spawnInterval;
+            }
+        }
+    }
+
+    void SpawnRandomObject(Vector3 spawnPosition)
+    {
+        // Get a random object from the array
+        GameObject randomObject = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+
+        // Instantiate the object at the spawn position with no rotation
+        Instantiate(randomObject, spawnPosition, Quaternion.identity);
+    }
+}
