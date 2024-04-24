58cddf162b24e99b70eaa05b630450706aa483e5
Apriana <aprianacahya04@gmail.com>
2024-04-24 15:41:59 +0700

    add info about bottle plastic

--- Assets/Scripts/Trash Collection/TrashInfo.cs
+++ Assets/Scripts/Trash Collection/TrashInfo.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TrashInfo : MonoBehaviour
 {
     public Trash[] trashes;
     public Sprite[] trashSprite;
     public GameObject[] ContentInfo;
+    public string[] SceneRecycleName;
 
     public Image trashImage;
     public Text titletxt;
     public Text categorytxt;
 
+    private int _selected;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +23,22 @@ public class TrashInfo : MonoBehaviour
 
     public void ShowTrashInfo(int index)
     {
+        _selected = index;
         gameObject.SetActive(true);
+        ContentInfo[index].SetActive(true);
         trashImage.sprite = trashSprite[index];
         titletxt.text = trashes[index].name;
         categorytxt.text = trashes[index].trashType;
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneRecycleName[_selected]);
+    }
+
+    private void OnDisable()
+    {
+        ContentInfo[_selected].SetActive(false);
     }
 
 }
